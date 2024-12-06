4954c93af66ffa0a825389527e0d1cbda87f4eca
j000 <jarymut+github@gmail.com>
2024-12-06 22:47:57 +0100

    [Day 1] Add solution

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,18 +1,36 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
+#include <fstream>
 #include <vector>
 
 int main(int argc, char** argv)
 {
-	std::vector<std::string> args{};
-	for (int i = 0; i < argc; ++i)
-	{
-		args.push_back(argv[i]);
+	std::ifstream fin{};
+
+	if (argc > 1) {
+		fin.open(argv[1]);
 	}
 
-	std::cout << "test\n";
-	for (auto str : args) {
-		std::cout << str << "\n";
+	std::istream &in = fin.is_open() ? fin : std::cin;
+
+	std::vector<long> left{};
+	std::vector<long> right{};
+
+	long a{0};
+	long b{0};
+	while (in >> a >> std::ws >> b) {
+		left.push_back(a);
+		right.push_back(b);
 	}
+
+	std::sort(left.begin(), left.end());
+	std::sort(right.begin(), right.end());
+
+	long long diff{0};
+	for (auto i = 0u; i < left.size(); ++i) {
+		diff += std::abs(right[i] - left[i]);
+	}
+	std::cout << diff << "\n";
+
 	return 0;
 }
